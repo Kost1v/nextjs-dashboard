22143ff28962e19f47054eaeeecf8f0fd750cbab
Ostap <ostapcostiv@gmail.com>
2024-12-10 13:48:33 +0200

    init project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "@/app/ui/global.css";
+import { inter } from "./ui/fonts";
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
